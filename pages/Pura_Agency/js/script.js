26355d5e87d01477da9f64148e9ccfde7e9f0998
sergei-dev <antonuk-sergei@rambler.ru>
2017-07-06 14:15:34 +0430

    ewewewew

--- pages/Pura_Agency/js/script.js
+++ pages/Pura_Agency/js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    $(window).scroll(function(){ 
+if($(this).scrollTop() > 50){ 
+$('.header_top_line').addClass('scroll_header_top_line'); 
+} 
+if($(this).scrollTop() < 50 && $('.header_top_line').hasClass('scroll_header_top_line')){ 
+$('.header_top_line').removeClass('scroll_header_top_line'); 
+} 
+});
+
+
+
 $('.menu_link_button').on('click',function(event){
 	 event.preventDefault();
 	    $('.modal_menu').toggleClass('modal_menu_active');
