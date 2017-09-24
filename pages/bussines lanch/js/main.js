2e96b57be27e27fc8062693c74205d8a765cd481
sergei-dev <antonuk-sergei@rambler.ru>
2017-09-24 19:13:21 +0330

    bussines lanch

--- pages/bussines lanch/js/main.js
+++ pages/bussines lanch/js/main.js
@@ -1,33 +1,44 @@
-$('.slick-main-container').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  fade: true,
-  asNavFor: '.slick-miniatures-container'
-});
-$('.slick-miniatures-container').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.slick-main-container',
-  dots: false,
-  arrows: false,
-  centerMode: true,
-  vertical: true,
-  verticalSwiping: true,
-  centerPadding: '0',
-  focusOnSelect: true,
-   responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        vertical: false,
-        verticalSwiping: false
+$(document).ready(function() {
+  $('.slick-main-container').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.slick-miniatures-container'
+  });
+  $('.slick-miniatures-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slick-main-container',
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    vertical: true,
+    verticalSwiping: true,
+    centerPadding: '0',
+    focusOnSelect: true,
+     responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          vertical: false,
+          verticalSwiping: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-      }
-    }
-  ]
+    ]
+  });
+
+  new WOW().init();
+
+  $('#tel').click(function() {
+    $('#required_label').toggle();
+  });
+  $('#tel').keypress(function() {
+    $('#required_label').hide();
+  })
 });
