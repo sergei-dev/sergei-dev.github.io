ea6a85d20314e80041c01ecf46a8dd2884e0fcbd
sergei-dev <antonuk-sergei@rambler.ru>
2017-12-01 16:36:21 +0330

    form

--- pages/app2/js/main.js
+++ pages/app2/js/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+	ymaps.ready(init);
+    var myMap,
+    myPlacemark;;
+
+    function init(){     
+        myMap = new ymaps.Map("Map", {
+            center: [59.9328,30.4179],
+            zoom: 16
+        });
+        myPlacemark = new ymaps.Placemark([59.9328,30.4179],
+         { hintContent: 'Финвал', balloonContent: 'Компания' });
+        myMap.geoObjects.add(myPlacemark);
+    }
+
+	 $('.nav nav a').on('click', function(e) {
+    var selector = $(this).attr('href');
+    var h = $(selector);
+    $('html, body').animate({
+      scrollTop: h.offset().top
+    }, 600);
+
+  });
+
 	 $(window).scroll(function() {
       if ($(this).scrollTop() > 50) {
             $('header').addClass('header_fixed');
@@ -17,5 +40,6 @@ $(document).ready(function() {
     },
     type: 'inline'
   });
-})
+
+});
 
