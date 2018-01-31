b35713a539f5f5b42e473fa483c24f0ecf0e6214
sergei-dev <antonuk-sergei@rambler.ru>
2018-01-31 16:10:14 +0100

    scrolls

--- pages/drm-mob/js/scroll.js
+++ pages/drm-mob/js/scroll.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
-	$('#categories-scroll1').mCustomScrollbar({axis:"x"}); 
-	$('#categories-scroll2').mCustomScrollbar({axis:"x",snapAmount: 2060});
-	$('#categories-scroll-city').mCustomScrollbar({axis:"x",snapAmount: 360});
-	$('#categories-scroll-weather-btn').mCustomScrollbar({axis:"x",snapAmount: 360});
-	$('#categories-scroll-weather-cards').mCustomScrollbar({axis:"x",snapAmount: 360});
+    $(".nav-tabs .content").mCustomScrollbar({
+        axis: 'x',
+        keyboard:{scrollType:"stepped"},
+        mouseWheel:{scrollAmount:188,normalizeDelta:true},
+        autoExpandScrollbar:true,
+        snapAmount:188,
+        snapOffset:65
+    });
 
+     $(".financial-news-agency-content .content").mCustomScrollbar({
+        axis: 'x',
+        mouseWheel:{scrollAmount:188,normalizeDelta:false},
+        snapAmount:400,
+        snapOffset:20
+    });
+
+     $("#exchange-rates-table-info .content").mCustomScrollbar({
+        axis: 'x',
+        mouseWheel:{scrollAmount:188,normalizeDelta:false},
+        snapAmount:400,
+        snapOffset:20
+    });
+     $("#weather-sect .content").mCustomScrollbar({
+        axis: 'x',
+        mouseWheel:{scrollAmount:188,normalizeDelta:false},
+        snapAmount:400,
+        snapOffset:20
+    });
+     $("#weather-sect .content:last-child").mCustomScrollbar({
+        axis: 'x',
+        mouseWheel:{scrollAmount:188,normalizeDelta:false},
+        snapAmount:400,
+        snapOffset:20
+    });
 });
