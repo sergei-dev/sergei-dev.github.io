3ab46093e7123f091f67efb77977ca21cd8beb49
sergei-dev <antonuk-sergei@rambler.ru>
2018-02-01 15:10:44 +0100

    drm pixsel

--- pages/drm-mob/js/scroll.js
+++ pages/drm-mob/js/scroll.js
@@ -1,39 +1,4 @@
 $(document).ready(function() {
-    function scrolls(element) { 
-    const slider = document.querySelector(element); 
-    let isDown = false; 
-    let startX; 
-    let scrollLeft; 
-
-    slider.addEventListener('mousedown', (e) => { 
-    isDown = true; 
-    slider.classList.add('active'); 
-    startX = e.pageX - slider.offsetLeft; 
-    scrollLeft = slider.scrollLeft; 
-    }); 
-
-    slider.addEventListener('mouseleave', () => { 
-    isDown = false; 
-    slider.classList.remove('active'); 
-    }); 
-
-    slider.addEventListener('mouseup', () => { 
-    isDown = false; 
-    slider.classList.remove('active'); 
-    }); 
-
-    slider.addEventListener('mousemove', (e) => { 
-    if (!isDown) return; 
-    e.preventDefault(); 
-    const x = e.pageX - slider.offsetLeft; 
-    const walk = (x - startX) * 3; 
-    slider.scrollLeft = scrollLeft - walk; 
-    }); 
-    } 
-
-    scrolls('#categories-scroll1');
-    scrolls('#categories-scroll2');
-    scrolls('#categories-scroll-city');
-    scrolls('#categories-scroll-weather-btn');
-    scrolls('#weather-scroll');
+	$('#categories-scroll1').mCustomScrollbar({axis:"x"}); 
+	$('#categories-scroll2').mCustomScrollbar({axis:"x",snapAmount: 360});
 });
