d6c014a510995d57228e77ce0cc6afd45461bb05
sergei-dev <antonuk-sergei@rambler.ru>
2018-02-03 15:00:24 +0100

    respons scroll

--- pages/drm-mob/js/scroll.js
+++ pages/drm-mob/js/scroll.js
@@ -1,39 +1,44 @@
 $(document).ready(function() {
-    function scrolls(element) { 
-    const slider = document.querySelector(element); 
-    let isDown = false; 
-    let startX; 
-    let scrollLeft; 
 
-    slider.addEventListener('mousedown', (e) => { 
-    isDown = true; 
-    slider.classList.add('active'); 
-    startX = e.pageX - slider.offsetLeft; 
-    scrollLeft = slider.scrollLeft; 
-    }); 
+    if (document.documentElement.clientWidth < 900) { 
+        function scrolls(element) { 
+            const slider = document.querySelector(element); 
+            let isDown = false; 
+            let startX; 
+            let scrollLeft; 
 
-    slider.addEventListener('mouseleave', () => { 
-    isDown = false; 
-    slider.classList.remove('active'); 
-    }); 
+            slider.addEventListener('mousedown', (e) => { 
+                isDown = true;  
+                startX = e.pageX - slider.offsetLeft; 
+                scrollLeft = slider.scrollLeft; 
+            }); 
 
-    slider.addEventListener('mouseup', () => { 
-    isDown = false; 
-    slider.classList.remove('active'); 
-    }); 
+            slider.addEventListener('mouseleave', () => { 
+                isDown = false; 
+            }); 
 
-    slider.addEventListener('mousemove', (e) => { 
-    if (!isDown) return; 
-    e.preventDefault(); 
-    const x = e.pageX - slider.offsetLeft; 
-    const walk = (x - startX) * 3; 
-    slider.scrollLeft = scrollLeft - walk; 
-    }); 
+            slider.addEventListener('mouseup', () => { 
+                isDown = false; 
+            }); 
+
+            slider.addEventListener('mousemove', (e) => { 
+                if (!isDown) return; 
+                e.preventDefault(); 
+                const x = e.pageX - slider.offsetLeft; 
+                const walk = (x - startX) * 3; 
+                slider.scrollLeft = scrollLeft - walk; 
+            }); 
+        } 
+
+        scrolls('.tabs-navigation ul');
+        scrolls('#categories-scroll2');
+        scrolls('#categories-scroll-city');
+        scrolls('#categories-scroll-weather-btn');
+        scrolls('#weather-scroll');
+
     } 
 
-    scrolls('.tabs-navigation ul');
-    scrolls('#categories-scroll2');
-    scrolls('#categories-scroll-city');
-    scrolls('#categories-scroll-weather-btn');
-    scrolls('#weather-scroll');
+
+    
+    
 });
