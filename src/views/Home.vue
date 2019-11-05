a70b100edadd6e24689ff4d6e9172fc920db8c7d
DESKTOP-D7G69AL\Сергей <sergefrontend@gmail.com>
2019-11-05 23:10:29 +0300

    puhs

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Sidebar></Sidebar>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SidebarApp from '@/components/SidebarApp.vue'
+
+export default {
+  name: 'home',
+  components: {
+    SidebarApp
+  }
+}
+</script>
