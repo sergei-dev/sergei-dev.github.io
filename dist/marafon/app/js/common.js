4971c29e0ec4d40b4c373a53de29509e48221a19
sergei-dev <antonuk-sergei@rambler.ru>
2020-04-29 11:22:22 +0300

    edits landing marafon

--- dist/marafon/app/js/common.js
+++ dist/marafon/app/js/common.js
@@ -1,12 +1,44 @@
 $(function() {
 
-	$('.js-simple-slider').slick({
-	lazyLoad: 'ondemand',
-	infinite: false,
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	arrows: true, 
-	dots: true,
-});
+	if ($('.js-simple-slider')) {
+		$('.js-simple-slider').slick({
+			lazyLoad: 'ondemand',
+			infinite: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: true, 
+			dots: true,
+		});
+	}
+
+	if ($('.js-popup-open')) {
+		$('.js-popup-open').on('click', (e) => {
+			e.preventDefault();
+			$('body').addClass('body-overflow');
+			$('.popup--registr').fadeIn();
+		});
+	}
+
+	// btn open popup
+
+	if ($('.js-popup-close')) {
+		$('.js-popup-close').on('click', (e) => {
+			e.preventDefault();
+			$('body').removeClass('body-overflow');
+			$('.popup').fadeOut();
+		});
+	}
+
+	// btn close popup
+
+	   $('.popup').mouseup(function(e) {
+        const content = $('.popup__content');
+        if (!content.is(e.target) && content.has(e.target).length === 0) {
+            $('.popup').fadeOut();
+            $('body').removeClass('body-overflow');
+        }
+    });
+
+	   // bg close popup
 
 });
